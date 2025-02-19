80d539445e018d416a53dddd4d69bf2b2375c89b
ShivaKrishnaReddyBurra <reddyburrashivakrishna@gmail.com>
2025-02-20 00:21:57 +0530

    update: Added few css styles

--- renderer.js
+++ renderer.js
@@ -1,7 +1,35 @@
+const loader = document.querySelector(".loader");
+const delay = +loader.dataset.delay || 200;
+const dots = loader.querySelectorAll(".loader .dot");
+dots.forEach((dot, index) => {
+  dot.style = `--delay: ${delay * index}`;
+});
+
 document.getElementById("start-btn").addEventListener("click", () => {
+    $('.loader').removeClass('d-none');
+    $('#start-btn').addClass('d-none');
     window.electron.startVoiceAssistant();
 });
 
 window.electron.onAssistantResponse((response) => {
-    document.getElementById("output").textContent += response + "\n";
+    document.getElementById("output").innerHTML += response + "<br>";
 });
+
+const outputElement = document.getElementById("output");
+
+const scrollToBottom = () => {
+    outputElement.scrollTop = outputElement.scrollHeight;
+};
+
+const observer = new MutationObserver(scrollToBottom);
+
+observer.observe(outputElement, { childList: true });
+
+outputElement.addEventListener("scroll", () => {
+    if (outputElement.scrollTop + outputElement.clientHeight >= outputElement.scrollHeight) {
+        observer.observe(outputElement, { childList: true });
+    } else {
+        observer.disconnect();
+    }
+});
+
